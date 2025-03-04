86b5d35c2a8e7b6332ba46cc40a38b668b1f41fb
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-03-04 18:08:45 +0800

    建立 category 的空白表格套件(category_form)、新增頁面（new.html）、controller

--- lib/artist_web/controllers/category_controller.ex
+++ lib/artist_web/controllers/category_controller.ex
@@ -10,4 +10,21 @@ defmodule ArtistWeb.CategoryController do
     categories = Categories.list_categories()
     render(conn, :index, categories: categories)
   end
+
+  def new(conn, _params) do
+    changeset = Categories.change_category(%Category{})
+    render(conn, :new, changeset: changeset)
+  end
+
+  def create(conn, %{"category" => category_params}) do
+    case Categories.create_category(category_params) do
+      {:ok, _category} ->
+        conn
+        |> put_flash(:info, "分類建立完成")
+        |> redirect(to: ~p"/admin/categories/")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+  end
 end
