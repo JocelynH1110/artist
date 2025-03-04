991962e34c0c874fc725794bb55a3915958738e9
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-03-04 23:47:57 +0800

    建立 category 更新頁面、controller

--- lib/artist_web/controllers/category_controller.ex
+++ lib/artist_web/controllers/category_controller.ex
@@ -33,4 +33,18 @@ defmodule ArtistWeb.CategoryController do
     changeset = Categories.change_category(category)
     render(conn, :edit, category: category, changeset: changeset)
   end
+
+  def update(conn, %{"id" => id, "category" => category_params}) do
+    category = Categories.get_category!(id)
+
+    case Categories.update_category(category, category_params) do
+      {:ok, _category} ->
+        conn
+        |> put_flash(:info, "分類更新成功")
+        |> redirect(to: ~p"/admin/categories/")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :edit, category: category, changeset: changeset)
+    end
+  end
 end
